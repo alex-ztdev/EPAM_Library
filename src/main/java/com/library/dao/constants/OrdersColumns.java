f208cc2c11e2fb02a12fb6d0b673bc1678ca0cd7
Alex Horobets <sgs.cat.fly@gmail.com>
2023-01-14 20:18:22 +0200

    added dao constants OrdersColumns, UserRolesColumns

---
+++ src/main/java/com/library/dao/constants/OrdersColumns.java
@@ -0,0 +1,10 @@
+package com.library.dao.constants;
+
+public interface OrdersColumns {
+    String ID = "id";
+    String USER_ID = "user_id";
+    String BOOK_ID = "book_id";
+    String ORDER_START_DATE = "order_start_date";
+    String SUBSCRIPTION_END_DATE = "subscription_end_date";
+    String ACTUAL_RETURN_DATE = "actual_return_date";
+}
---
+++ src/main/java/com/library/dao/constants/UserRolesColumns.java
@@ -0,0 +1,6 @@
+package com.library.dao.constants;
+
+public interface UserRolesColumns {
+    String ID = "id";
+    String TITLE = "title";
+}
