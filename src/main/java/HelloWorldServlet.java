c139d20595c2538c85e39eef161b3add6fbe3040
Alex Horobets <sgs.cat.fly@gmail.com>
2023-01-14 22:45:05 +0200

    added ConnectionPool, ConnectionProxy

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,11 +1,14 @@
 import com.library.connection_pool.DBManager;
+import com.library.utils.PropertiesUtil;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.UUID;
 
 public class HelloWorldServlet {
     public static void main(String[] args) {
-        var db = DBManager.getInstance();
+
     }
 }
--- src/main/java/com/library/connection_pool/DBManager.java
+++ src/main/java/com/library/connection_pool/DBManager.java
@@ -1,5 +1,6 @@
 package com.library.connection_pool;
 
+import com.library.exceptions.DBManagerException;
 import com.library.utils.PropertiesUtil;
 
 import java.sql.Connection;
@@ -12,19 +13,30 @@ public class DBManager {
 
     private static volatile DBManager dbm;
     private static final String URL = "db.url";
+    private static final String USERNAME = "db.username";
+    private static final String PASSWORD = "db.password";
     private static final String POOL_SIZE = "db.pool.size";
+
 
     private static final Integer DEFAULT_POOL_SIZE = 10;
     private BlockingQueue<Connection> pool;
 
 
     private void initConnectionPool() {
-        var poolSizeProperty = PropertiesUtil.get(POOL_SIZE);
-        var poolSize = poolSizeProperty == null ? DEFAULT_POOL_SIZE : Integer.parseInt(POOL_SIZE);
+        var poolSizePropertyVal = PropertiesUtil.get(POOL_SIZE);
+        var poolSize = poolSizePropertyVal == null ? DEFAULT_POOL_SIZE : Integer.parseInt(poolSizePropertyVal);
         pool = new ArrayBlockingQueue<>(poolSize);
 
-        for (int i = 0; i < poolSize; i++) {
-            pool.add(open());
+
+        try {
+            for (int i = 0; i < poolSize; i++) {
+                pool.add(new ConnectionProxy(DriverManager.getConnection(PropertiesUtil.get(URL),
+                        PropertiesUtil.get(USERNAME),
+                        PropertiesUtil.get(PASSWORD)),
+                        pool));
+            }
+        } catch (SQLException e) {
+            throw new DBManagerException(e);
         }
     }
 
@@ -45,19 +57,20 @@ public class DBManager {
         }
     }
 
-    private Connection open() {
-        try {
-            return DriverManager.getConnection(PropertiesUtil.get(URL));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    private Connection open() {
+//        try {
+//
+//            return;
+//        } catch (SQLException e) {
+//            throw new DBManagerException(e);
+//        }
+//    }
 
     public Connection get() {
         try {
             return pool.take();
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            throw new DBManagerException(e);
         }
     }
 }
