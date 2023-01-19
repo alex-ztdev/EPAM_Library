8613d3b760986e154f96ff4da2cdd2e4d0a8f206
Alex Horobets <sgs.cat.fly@gmail.com>
2023-01-19 19:15:57 +0200

    configured tomcat

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,76 +1,16 @@
-import com.my.library.connection_pool.ConnectionPool;
-import com.my.library.dao.BookDAO;
-import com.my.library.dao.OrderDAO;
-import com.my.library.dao.constants.UserRole;
-import com.my.library.dao.constants.UserStatus;
-import com.my.library.dao.impl.AuthorDaoImpl;
-import com.my.library.dao.impl.BookDaoImpl;
-import com.my.library.dao.impl.OrderDaoImpl;
-import com.my.library.dao.impl.UserDaoImpl;
-import com.my.library.entities.Author;
-import com.my.library.entities.Book;
-import com.my.library.entities.Order;
-import com.my.library.entities.User;
-import com.my.library.exceptions.DaoException;
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Properties;
+import java.io.IOException;
 
-public class HelloWorldServlet {
-    private static final Logger logger = LogManager.getLogger();
+@WebServlet("/hi")
+public class HelloWorldServlet extends HttpServlet {
 
-    public static void main(String[] args) {
-        try {
-            try {
-                var bookDao = BookDaoImpl.getInstance();
-//                bookDao.find(13).ifPresent(System.out::println);
-//                bookDao.findAll().forEach(x-> System.out.println(x));
-//                var book = new Book(101L, "My title UPDATED 5.0!", "Hachette", "Satire", 129, LocalDate.now(), false, false);
-//                System.out.println(bookDao.find(101L));
-//
-//                bookDao.save(book);
-//                bookDao.findAll().forEach(System.out::println);
-//                bookDao.update(book);
-//                System.out.println(bookDao.find(book.getBookId()));
-//
-//
-//                System.out.println(bookDao.find(10));
-//
-//                bookDao.delete(10);
-//                System.out.println(bookDao.find(10));
-//                bookDao.getBookAuthors(1).forEach(System.out::println);
-                OrderDaoImpl orderDao = OrderDaoImpl.getInstance();
-
-//                orderDao.findAll().forEach(System.out::println);
-                UserDaoImpl userDao = UserDaoImpl.getInstance();
-//                var order = new Order(userDao.find(10000).get(),
-//                        bookDao.find(13).get(),
-//                        LocalDateTime.of(2000, 2, 23, 19, 1),
-//                        LocalDateTime.of(2000, 3, 23, 19, 1),
-//                        null
-//                );
-//                orderDao.save(order);
-//                System.out.println(order);
-//                order.setActualReturnDate(LocalDateTime.now());
-//                orderDao.delete(order);
-//                orderDao.find(order.getOrderId()).ifPresent(System.out::println);
-
-                orderDao.getUsersOrders(10004).forEach(System.out::println);
-
-            } catch (DaoException e) {
-                throw new RuntimeException(e);
-            }
-
-        } finally {
-            ConnectionPool.getInstance().destroyPool();
-        }
-
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hi");
     }
 }
--- src/main/java/com/my/library/services/UserService.java
+++ src/main/java/com/my/library/services/UserService.java
@@ -3,8 +3,8 @@ package com.my.library.services;
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class UserService {
-    public static void main(String[] args) {
-        String sha256hex = DigestUtils.sha512Hex("admin");
-        System.out.println(sha256hex);
-    }
+//    public static void main(String[] args) {
+//        String sha256hex = DigestUtils.sha512Hex("admin");
+//        System.out.println(sha256hex);
+//    }
 }
