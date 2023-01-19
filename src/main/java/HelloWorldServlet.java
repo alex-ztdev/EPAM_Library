c372ea242688ef54efbeaa66229334a436164b8f
Alex Horobets <sgs.cat.fly@gmail.com>
2023-01-19 20:27:31 +0200

    configured tomcat

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,3 +1,8 @@
+import com.my.library.connection_pool.ConnectionPool;
+import com.my.library.dao.impl.AuthorDaoImpl;
+import com.my.library.dao.impl.UserDaoImpl;
+import com.my.library.exceptions.DaoException;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,9 +13,25 @@ import java.io.IOException;
 
 @WebServlet("/hi")
 public class HelloWorldServlet extends HttpServlet {
+    AuthorDaoImpl authorDao = AuthorDaoImpl.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("Hi");
+
+        try {
+
+            resp.getWriter().append(authorDao.find(1).get().toString());
+
+        } catch (DaoException e) {
+
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        ConnectionPool.getInstance().destroyPool();
+        super.destroy();
     }
 }
