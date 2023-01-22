573534ebde7fe22d257f6e157dc9920af4444176
Alex Horobets <sgs.cat.fly@gmail.com>
2023-01-22 16:41:57 +0200

    added 404.jsp error page

--- src/main/java/com/my/library/utils/Pages.java
+++ src/main/java/com/my/library/utils/Pages.java
@@ -3,6 +3,6 @@ package com.my.library.utils;
 public interface Pages {
     String LOGIN_PAGE = "/login.jsp";
     String MAIN_PAGE = "/index.jsp";
-    String ERROR_PAGE = "pages/error_pages/pageNotFound.jsp";
+    String ERROR_PAGE = "pages/error/404.jsp";
     String PROFILE_PAGE = "/pages/profile.jsp";
 }
