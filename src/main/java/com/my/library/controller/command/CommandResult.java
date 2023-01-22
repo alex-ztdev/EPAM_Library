38e8433fd9f34eacd9ed0324f7eec20177cfc13a
Alex Horobets <sgs.cat.fly@gmail.com>
2023-01-22 12:03:17 +0200

    implemented CommandResult

--- src/main/java/com/my/library/controller/command/CommandResult.java
+++ src/main/java/com/my/library/controller/command/CommandResult.java
@@ -4,5 +4,30 @@ import com.my.library.controller.command.constant.CommandDirection;
 
 public class CommandResult {
     private String page;
-    private CommandDirection action;
+    private CommandDirection action = CommandDirection.FORWARD; //Direction is forward by default
+
+    public CommandResult(String page) {
+        this.page = page;
+    }
+
+    public CommandResult(String page, CommandDirection action) {
+        this.page = page;
+        this.action = action;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public CommandDirection getAction() {
+        return action;
+    }
+
+    public void setAction(CommandDirection action) {
+        this.action = action;
+    }
 }
