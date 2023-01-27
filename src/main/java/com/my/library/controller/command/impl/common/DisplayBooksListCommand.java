60b3d06a47bd79a54501f270c29054460133aef8
Alex Horobets <sgs.cat.fly@gmail.com>
2023-01-27 23:09:27 +0200

    added pagination to DisplayBooksListCommand

--- src/main/java/com/my/library/controller/command/impl/common/DisplayBooksListCommand.java
+++ src/main/java/com/my/library/controller/command/impl/common/DisplayBooksListCommand.java
@@ -17,25 +17,43 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public class DisplayBooksListCommand implements Command {
-    private static final int RECORDS_PER_PAGE = 20;
+    private static final int RECORDS_PER_PAGE = 15;
 
     @Override
     public CommandResult execute(HttpServletRequest request) throws CommandException {
         BookService bookService = ServiceFactory.getBookService();
         int currPage = 1;
 
+        BooksOrderDir orderDir = BooksOrderDir.ASC;
+        BooksOrderTypes orderBy = BooksOrderTypes.BY_TITLE;
+
         var reqCurrPage = request.getParameter(Parameters.BOOKS_LIST_CURR_PAGE);
+        var reqOrderDir = request.getParameter(Parameters.ORDER_DIRECTION);
+        var reqOrderBy = request.getParameter(Parameters.ORDER_BY);
 
         if (reqCurrPage != null && !reqCurrPage.isBlank()) {
             currPage = Integer.parseInt(reqCurrPage);
         }
+        if (reqOrderDir != null && !reqOrderDir.isBlank()) {
+            orderDir = BooksOrderDir.valueOf(reqOrderDir);
+        }
+        if (reqOrderBy != null && !reqOrderBy.isBlank()) {
+            orderBy = BooksOrderTypes.valueOf(reqOrderBy);
+        }
 
         try {
             List<Book> booksList = bookService.findAll((currPage - 1) * RECORDS_PER_PAGE,
-                    RECORDS_PER_PAGE,
-                    BooksOrderTypes.BY_TITLE,
-                    BooksOrderDir.ASC);
+                    RECORDS_PER_PAGE, orderBy, orderDir
+            );
+            int totalRecords = bookService.countBooks();
+            int totalPages = (int) Math.ceil((double) totalRecords / RECORDS_PER_PAGE);
+
+            request.setAttribute(Parameters.BOOKS_LIST_CURR_PAGE, currPage);
+            request.setAttribute(Parameters.ORDER_DIRECTION, orderDir.toString());
+            request.setAttribute(Parameters.ORDER_BY, orderBy.toString());
+            request.setAttribute(Parameters.BOOKS_TOTAL_PAGES, totalPages);
             request.setAttribute(Parameters.BOOKS_LIST, booksList);
+
         } catch (ServiceException e) {
             throw new CommandException(e);
         }
