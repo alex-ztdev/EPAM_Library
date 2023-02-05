45fbe88610d97cdbbbf0c0757532e3003855898b
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-05 20:53:09 +0200

    added check for null in IntegerParser and LongParser

--- src/main/java/com/my/library/utils/IntegerParser.java
+++ src/main/java/com/my/library/utils/IntegerParser.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 public class IntegerParser {
 
     public static Optional<Integer> parseInt(String number) {
+        if (number == null) {
+            return Optional.empty();
+        }
         try {
             return Optional.of(Integer.parseInt(number));
         } catch (NumberFormatException e) {
--- src/main/java/com/my/library/utils/LongParser.java
+++ src/main/java/com/my/library/utils/LongParser.java
@@ -4,6 +4,9 @@ import java.util.Optional;
 
 public class LongParser {
     public static Optional<Long> parseLong(String number) {
+        if (number == null) {
+            return Optional.empty();
+        }
         try {
             return Optional.of(Long.parseLong(number));
         } catch (NumberFormatException e) {
