a63ca1cddbd49b33e9cfa0473fea020463e642dc
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-08 23:36:22 +0200

    added IntegerParserTest

---
+++ src/test/java/com/my/library/utils/IntegerParserTest.java
@@ -0,0 +1,18 @@
+package com.my.library.utils;
+
+import com.my.library.controller.command.constant.parameters.UserParameters;
+import com.my.library.entities.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IntegerParserTest {
+
+}
