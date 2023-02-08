389dee0e9587ebefa775781853d7e1892e528a8b
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-08 22:47:48 +0200

    removed redundant code

--- src/main/java/com/my/library/utils/validator/UserValidator.java
+++ src/main/java/com/my/library/utils/validator/UserValidator.java
@@ -12,39 +12,6 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class UserValidator {
-    public List<String> validateUserParameters(HttpServletRequest request) {
-
-        List<String> validationList = new ArrayList<>();
-        String firstName = request.getParameter(UsersColumns.FIRST_NAME);
-        String lastName = request.getParameter(UsersColumns.SECOND_NAME);
-        String email = request.getParameter(UsersColumns.EMAIL);
-        String login = request.getParameter(UsersColumns.LOGIN);
-        String password = request.getParameter(UsersColumns.PASSWORD);
-
-//        if(firstName.isEmpty() || !isValidName(firstName)){
-//            validationList.add(DiffConstant.NAME_ERROR);
-//        }
-//
-//        if(lastName.isEmpty() || !isValidName(lastName)){
-//            validationList.add(DiffConstant.LAST_NAME_ERROR);
-//        }
-//
-//        if(email.isEmpty() || !isValidEmail(email)){
-//            validationList.add(DiffConstant.EMAIL_ERROR);
-//        }
-//
-//        if(login.isEmpty() || !isValidLogin(login)){
-//            validationList.add(DiffConstant.LOGIN_ERROR);
-//        }
-//
-//        if(password != null){ // Not null means new user, null means old user just we update his is data.
-//            if(password.isEmpty() || !isValidPassword(password)){
-//                validationList.add(DiffConstant.PASSWORD_ERROR);
-//            }
-//        }
-
-        return validationList;
-    }
 
     public List<String> validateUserParameters(User user) {
         List<String> validationList = new ArrayList<>();
