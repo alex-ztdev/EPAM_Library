a4910c4ac0efe1ba6c4dc58e4c2375837c9e96c0
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-08 23:45:12 +0200

    tested parseInt method

--- src/test/java/com/my/library/utils/IntegerParserTest.java
+++ src/test/java/com/my/library/utils/IntegerParserTest.java
@@ -9,10 +9,36 @@ import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class IntegerParserTest {
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    public void parseInt_NullInput_ReturnsEmptyOptional(String input) {
+        Optional<Integer> result = IntegerParser.parseInt(input);
+        assertThat(result).isNotPresent();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "-123", "100", "123456"})
+    void parseInt_ValidInput_ReturnsOptionalOfNumber(String input) {
+        Optional<Integer> result = IntegerParser.parseInt(input);
+
+        assertThat(result)
+                .isPresent()
+                .contains(Integer.valueOf(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc", "-234324432324234324324", "ghi", "jkl", "2349.230"})
+    void parseInt_NotANumberInput_ReturnEmptyOptional(String input) {
+        Optional<Integer> result = IntegerParser.parseInt(input);
+
+        assertThat(result).isNotPresent();
+    }
 
 }
