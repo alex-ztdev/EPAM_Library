89e6897228d97fd7bc639625329eb50a78a15929
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-09 17:41:30 +0200

    added test for removeRegistrationErrors and removeBookErrors methods

--- src/test/java/com/my/library/utils/validator/MessagesRemoverTest.java
+++ src/test/java/com/my/library/utils/validator/MessagesRemoverTest.java
@@ -1,19 +1,15 @@
 package com.my.library.utils.validator;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import com.my.library.controller.command.constant.parameters.BookParameters;
+import com.my.library.controller.command.constant.parameters.Parameters;
 import com.my.library.controller.command.constant.parameters.UserParameters;
 import jakarta.servlet.http.HttpSession;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,17 +22,38 @@ public class MessagesRemoverTest {
     @BeforeEach
     public void setUp() {
         messagesRemover = new MessagesRemover();
-
-
     }
     @Test
     public void removeLoginErrors_ShouldRemoveCorrectAttributesFromSession() {
-//        when(session.getAttribute(UserParameters.INVALID_LOGIN_PASSWORD)).thenReturn("invalidLoginPassword");
-//        when(session.getAttribute(UserParameters.USER_IS_BLOCKED)).thenReturn("isBlocked");
-
         messagesRemover.removeLoginErrors(session);
 
         verify(session, times(1)).removeAttribute(UserParameters.INVALID_LOGIN_PASSWORD);
         verify(session,  times(1)).removeAttribute(UserParameters.USER_IS_BLOCKED);
     }
+
+    @Test
+    public void removeBookErrors_ShouldRemoveCorrectAttributesFromSession() {
+        messagesRemover.removeBookErrors(session);
+
+        verify(session, times(1)).removeAttribute(BookParameters.BOOK_INVALID_DATA);
+        verify(session, times(1)).removeAttribute(BookParameters.BOOK_ALREADY_EXISTS);
+        verify(session, times(1)).removeAttribute(BookParameters.SUCCESSFULLY_UPDATED);
+        verify(session, times(1)).removeAttribute(BookParameters.SUCCESSFULLY_ADDED);
+        verify(session, times(1)).removeAttribute(Parameters.UPDATE_BOOK);
+        verify(session, times(1)).removeAttribute(Parameters.ADD_BOOK);
+    }
+    @Test
+    public void removeRegistrationErrors_ShouldRemoveCorrectAttributesFromSession() {
+        messagesRemover.removeRegistrationErrors(session);
+
+        verify(session, times(1)).removeAttribute(UserParameters.USER_EMAIL_ALREADY_EXISTS);
+        verify(session, times(1)).removeAttribute(UserParameters.USER_LOGIN_ALREADY_EXISTS);
+        verify(session, times(1)).removeAttribute(UserParameters.USER_PHONE_ALREADY_EXISTS);
+        verify(session, times(1)).removeAttribute(UserParameters.REG_LOGIN_VAL);
+        verify(session, times(1)).removeAttribute(UserParameters.REG_EMAIL_VAL);
+        verify(session, times(1)).removeAttribute(UserParameters.REG_PHONE_VAL);
+        verify(session, times(1)).removeAttribute(UserParameters.REG_FIRST_NAME_VAL);
+        verify(session, times(1)).removeAttribute(UserParameters.REG_SECOND_NAME_VAL);
+    }
+
 }
