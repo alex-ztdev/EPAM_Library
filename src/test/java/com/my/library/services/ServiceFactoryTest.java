acd08fa58523813ce02bd383382374c28211c98f
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-09 15:12:30 +0200

    removed redundant code, optimized imports

--- src/test/java/com/my/library/services/ServiceFactoryTest.java
+++ src/test/java/com/my/library/services/ServiceFactoryTest.java
@@ -1,52 +1,27 @@
 package com.my.library.services;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.assertj.core.api.Assertions.assertThat;
+import com.my.library.dao.DaoFactory;
+import com.my.library.services.impl.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.sql.Connection;
 
-import com.my.library.dao.impl.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import com.my.library.dao.DaoFactory;
-import com.my.library.dao.GenreDAO;
-import com.my.library.dao.OrderDAO;
-import com.my.library.services.BookService;
-import com.my.library.services.GenreService;
-import com.my.library.services.OrderService;
-import com.my.library.services.PublisherService;
-import com.my.library.services.UserService;
-import com.my.library.services.impl.AuthorServiceImpl;
-import com.my.library.services.impl.BookServiceImpl;
-import com.my.library.services.impl.GenreServiceImpl;
-import com.my.library.services.impl.OrderServiceImpl;
-import com.my.library.services.impl.PublisherServiceImpl;
-import com.my.library.services.impl.UserServiceImpl;
-import org.mockito.Mock;
-
+import static org.assertj.core.api.Assertions.assertThat;
+@ExtendWith(MockitoExtension.class)
 public class ServiceFactoryTest {
 
     @Mock
     private Connection connection;
     @Mock
     private DaoFactory daoFactory;
+
+    @InjectMocks
     private ServiceFactory serviceFactory;
 
-    @BeforeEach
-    public void setUp() {
-        connection = mock(Connection.class);
-        daoFactory = mock(DaoFactory.class);
-        when(daoFactory.getUserDao()).thenReturn(mock(UserDaoImpl.class));
-        when(daoFactory.getBookDao()).thenReturn(mock(BookDaoImpl.class));
-        when(daoFactory.getAuthorDao()).thenReturn(mock(AuthorDaoImpl.class));
-        when(daoFactory.getPublishersDao()).thenReturn(mock(PublisherDaoImpl.class));
-        when(daoFactory.getGenreDao()).thenReturn(mock(GenreDaoImpl.class));
-        when(daoFactory.getOrderDao()).thenReturn(mock(OrderDAO.class));
-
-        serviceFactory = new ServiceFactory(connection);
-    }
 
     @Test
     public void testGetUserService() {
