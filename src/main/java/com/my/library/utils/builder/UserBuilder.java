1ad1c365b4666eef5aa33b7063247cc12e774f21
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-11 01:10:17 +0200

    removed unused method

--- src/main/java/com/my/library/utils/builder/UserBuilder.java
+++ src/main/java/com/my/library/utils/builder/UserBuilder.java
@@ -10,22 +10,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 public class UserBuilder {
-    //TODO: implement buildUserForUpdate
-    public User buildUserForUpdate(HttpServletRequest request) {
-        String id = request.getParameter(UsersColumns.ID);
-        String firstName = request.getParameter(UsersColumns.FIRST_NAME);
-        String secondName = request.getParameter(UsersColumns.SECOND_NAME);
-        String email = request.getParameter(UsersColumns.EMAIL);
-        String login = request.getParameter(UsersColumns.LOGIN);
-        String phoneNumber = request.getParameter(UsersColumns.PHONE_NUMBER);
-        //UserRole role = request.getParameter(UsersColumns.ROLE_ID); //TODO: implement rolesFactory
-//        String blocked = request.getParameter(UsersColumns.SY);
-//        String blocked = request.getParameter(UsersColumns.BIRTH_DATE);
-
-//        return new User(Long.parseLong(id), name, lastName, email, login, role, Boolean.parseBoolean(blocked.trim()));
-        return null;
-    }
-
     public Optional<User> buildNewUser(HttpServletRequest request) {
         String login = request.getParameter(UserParameters.REG_LOGIN);
         String email = request.getParameter(UserParameters.REG_EMAIL);
