f762a297c5e0c08273c4f16df4f3d266b7375317
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-10 20:50:16 +0200

    added UserBuilderTest

--- src/test/java/com/my/library/utils/builder/UserBuilderTest.java
+++ src/test/java/com/my/library/utils/builder/UserBuilderTest.java
@@ -29,7 +29,7 @@ class UserBuilderTest {
     void buildNewUser_emptyOrNullLogin_ShouldReturnEmptyOptional(String login) {
         when(request.getParameter(UserParameters.REG_LOGIN)).thenReturn(login);
         when(request.getParameter(UserParameters.REG_EMAIL)).thenReturn("email@email.com");
-        when(request.getParameter(UserParameters.REG_PHONE)).thenReturn("");
+        when(request.getParameter(UserParameters.REG_PHONE)).thenReturn("380777777777");
         when(request.getParameter(UserParameters.REG_FIRST_NAME)).thenReturn("John");
         when(request.getParameter(UserParameters.REG_SECOND_NAME)).thenReturn("Doe");
         when(request.getParameter(UserParameters.REG_PASSWORD)).thenReturn("password");
@@ -39,4 +39,82 @@ class UserBuilderTest {
 
         assertThat(optionalUser).isEmpty();
     }
+
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    void buildNewUser_emptyOrNullEmail_ShouldReturnEmptyOptional(String email) {
+        when(request.getParameter(UserParameters.REG_LOGIN)).thenReturn("login");
+        when(request.getParameter(UserParameters.REG_EMAIL)).thenReturn(email);
+        when(request.getParameter(UserParameters.REG_PHONE)).thenReturn("380777777777");
+        when(request.getParameter(UserParameters.REG_FIRST_NAME)).thenReturn("John");
+        when(request.getParameter(UserParameters.REG_SECOND_NAME)).thenReturn("Doe");
+        when(request.getParameter(UserParameters.REG_PASSWORD)).thenReturn("password");
+        when(request.getParameter(UserParameters.REG_CONF_PASSWORD)).thenReturn("password");
+
+        Optional<User> optionalUser = userBuilder.buildNewUser(request);
+
+        assertThat(optionalUser).isEmpty();
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    void buildNewUser_emptyOrNullPhone_ShouldReturnOptionalOfUser(String phone) {
+        when(request.getParameter(UserParameters.REG_LOGIN)).thenReturn("validLogin");
+        when(request.getParameter(UserParameters.REG_EMAIL)).thenReturn("validEmail@email.com");
+        when(request.getParameter(UserParameters.REG_PHONE)).thenReturn(phone);
+        when(request.getParameter(UserParameters.REG_FIRST_NAME)).thenReturn("validFirstName");
+        when(request.getParameter(UserParameters.REG_SECOND_NAME)).thenReturn("validSecondName");
+        when(request.getParameter(UserParameters.REG_PASSWORD)).thenReturn("password");
+        when(request.getParameter(UserParameters.REG_CONF_PASSWORD)).thenReturn("password");
+
+        Optional<User> optionalUser = userBuilder.buildNewUser(request);
+
+        assertThat(optionalUser).isPresent();
+
+        User user = optionalUser.get();
+
+        assertThat(user.getLogin()).isEqualTo("validLogin");
+        assertThat(user.getEmail()).isEqualTo("validEmail@email.com");
+        assertThat(user.getFirstName()).isEqualTo("validFirstName");
+        assertThat(user.getSecondName()).isEqualTo("validSecondName");
+        assertThat(user.getPassword()).isEqualTo("password");
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    void buildNewUser_emptyOrNullFirstName_ShouldReturnEmptyOptional(String firstName) {
+        when(request.getParameter(UserParameters.REG_LOGIN)).thenReturn("validLogin");
+        when(request.getParameter(UserParameters.REG_EMAIL)).thenReturn("validEmail@email.com");
+        when(request.getParameter(UserParameters.REG_PHONE)).thenReturn("380777777777");
+        when(request.getParameter(UserParameters.REG_FIRST_NAME)).thenReturn(firstName);
+        when(request.getParameter(UserParameters.REG_SECOND_NAME)).thenReturn("validSecondName");
+        when(request.getParameter(UserParameters.REG_PASSWORD)).thenReturn("password");
+        when(request.getParameter(UserParameters.REG_CONF_PASSWORD)).thenReturn("password");
+
+        Optional<User> optionalUser = userBuilder.buildNewUser(request);
+
+        assertThat(optionalUser).isEmpty();
+    }
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    void buildNewUser_emptyOrNullSecondName_ShouldReturnEmptyOptional(String secondName) {
+        when(request.getParameter(UserParameters.REG_LOGIN)).thenReturn("validLogin");
+        when(request.getParameter(UserParameters.REG_EMAIL)).thenReturn("validEmail@email.com");
+        when(request.getParameter(UserParameters.REG_PHONE)).thenReturn("380777777777");
+        when(request.getParameter(UserParameters.REG_FIRST_NAME)).thenReturn("validFirstName");
+        when(request.getParameter(UserParameters.REG_SECOND_NAME)).thenReturn(secondName);
+        when(request.getParameter(UserParameters.REG_PASSWORD)).thenReturn("password");
+        when(request.getParameter(UserParameters.REG_CONF_PASSWORD)).thenReturn("password");
+
+        Optional<User> optionalUser = userBuilder.buildNewUser(request);
+
+        assertThat(optionalUser).isEmpty();
+    }
+
+
+
 }
