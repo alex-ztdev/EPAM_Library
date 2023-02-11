331c72e3bcb3fca5ada82e2cc01978ccc02b158c
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-12 00:13:35 +0200

    changed junit to assertJ assertions

--- src/test/java/com/my/library/services/impl/PublisherServiceImplTest.java
+++ src/test/java/com/my/library/services/impl/PublisherServiceImplTest.java
@@ -13,7 +13,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,13 +35,13 @@ class PublisherServiceImplTest {
 
         List<Publisher> actualPublishers = publisherServiceImpl.findAll();
 
-        assertEquals(expectedPublishers, actualPublishers);
+        assertThat(actualPublishers).isEqualTo(expectedPublishers);
     }
 
     @Test
     public void findAll_WhenDaoExceptionIsThrown_ShouldThrowServiceException() throws DaoException {
         when(publisherDAO.findAll()).thenThrow(new DaoException("Error while finding publishers"));
 
-        assertThrows(ServiceException.class, () -> publisherServiceImpl.findAll());
+        assertThatThrownBy(() -> publisherServiceImpl.findAll()).isExactlyInstanceOf(ServiceException.class);
     }
 }
