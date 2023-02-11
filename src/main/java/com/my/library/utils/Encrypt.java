7426160fcd7cd81e1b9969ceaec328170ee4f99a
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-11 17:58:48 +0200

    changed method accessibility to public

--- src/main/java/com/my/library/utils/Encrypt.java
+++ src/main/java/com/my/library/utils/Encrypt.java
@@ -3,7 +3,7 @@ package com.my.library.utils;
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class Encrypt {
-    private static String encryptWithSha512Hex(String password) {
+    public static String encryptWithSha512Hex(String password) {
         return DigestUtils.sha512Hex(password);
     }
 }
