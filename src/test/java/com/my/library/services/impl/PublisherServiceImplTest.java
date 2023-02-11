b627ff9beeceaa14420b1c6cc8e7c82e88f62dae
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-12 00:10:38 +0200

    added tests for PublisherServiceImpl

---
+++ src/test/java/com/my/library/services/impl/PublisherServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.my.library.services.impl;
+
+import com.my.library.dao.PublisherDAO;
+import com.my.library.entities.Publisher;
+import com.my.library.exceptions.DaoException;
+import com.my.library.exceptions.ServiceException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PublisherServiceImplTest {
+
+    @Mock
+    private PublisherDAO publisherDAO;
+
+    @InjectMocks
+    private PublisherServiceImpl publisherServiceImpl;
+
+    @Test
+    public void findAll_ShouldReturnListOfPublishers() throws ServiceException, DaoException {
+        List<Publisher> expectedPublishers = Arrays.asList(new Publisher(1L, "Publisher1"),
+                new Publisher(2L, "Publisher2"));
+
+        when(publisherDAO.findAll()).thenReturn(expectedPublishers);
+
+        List<Publisher> actualPublishers = publisherServiceImpl.findAll();
+
+        assertEquals(expectedPublishers, actualPublishers);
+    }
+
+    @Test
+    public void findAll_WhenDaoExceptionIsThrown_ShouldThrowServiceException() throws DaoException {
+        when(publisherDAO.findAll()).thenThrow(new DaoException("Error while finding publishers"));
+
+        assertThrows(ServiceException.class, () -> publisherServiceImpl.findAll());
+    }
+}
