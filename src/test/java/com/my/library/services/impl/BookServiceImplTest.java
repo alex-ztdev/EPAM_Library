d82298c0484cc057fc5658d17f4b087cf3d2a383
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-13 00:38:47 +0200

    added tests for findAll method

--- src/test/java/com/my/library/services/impl/BookServiceImplTest.java
+++ src/test/java/com/my/library/services/impl/BookServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.my.library.services.impl;
 
+import com.my.library.controller.command.constant.OrderDir;
 import com.my.library.dao.BookDAO;
+import com.my.library.dao.constants.BooksOrderTypes;
 import com.my.library.entities.Book;
 import com.my.library.exceptions.DaoException;
 import com.my.library.exceptions.ServiceException;
@@ -12,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,7 +34,7 @@ class BookServiceImplTest {
     @DisplayName("deleteById")
     class DeleteById {
         @Test
-        void deleteById_SuccessfulDeletion_ShouldCallDeleteByIdOnDAOAndReturnTrue() throws ServiceException, DaoException, ServiceException, DaoException {
+        void deleteById_SuccessfulDeletion_ShouldCallDeleteByIdOnDAOAndReturnTrue() throws ServiceException, DaoException {
             long id = 1L;
             doReturn(true).when(bookDAO).deleteById(id);
 
@@ -102,6 +105,40 @@ class BookServiceImplTest {
                     .hasCauseExactlyInstanceOf(DaoException.class);
 
             verify(bookDAO, times(1)).find(id);
+        }
+    }
+
+    @Nested
+    @DisplayName("find")
+    class findAll {
+        @Test
+        void findAll_ReturnsCorrectBooks() throws DaoException, ServiceException {
+            Book book = mock(Book.class);
+            List<Book> expectedBooks = List.of(book, book, book);
+
+            int start = 0;
+            int offset = 5;
+
+            when(bookDAO.findAll(start, offset, BooksOrderTypes.BY_ID, OrderDir.ASC, false)).thenReturn(expectedBooks);
+
+            List<Book> actualBooks = bookServiceImpl.findAll(start, offset, BooksOrderTypes.BY_ID, OrderDir.ASC, false);
+
+            assertThat(actualBooks).isEqualTo(expectedBooks);
+            verify(bookDAO).findAll(start, offset, BooksOrderTypes.BY_ID, OrderDir.ASC, false);
+        }
+
+        @Test
+        void findAll_BookDaoExceptionThrown_ShouldThrowServiceException() throws DaoException, ServiceException {
+            int start = 0;
+            int offset = 5;
+
+            doThrow(DaoException.class).when(bookDAO).findAll(start, offset, BooksOrderTypes.BY_ID, OrderDir.ASC, false);
+
+            assertThatThrownBy(() -> bookServiceImpl.findAll(start, offset, BooksOrderTypes.BY_ID, OrderDir.ASC, false))
+                    .isExactlyInstanceOf(ServiceException.class)
+                    .hasCauseExactlyInstanceOf(DaoException.class);
+
+            verify(bookDAO).findAll(start, offset, BooksOrderTypes.BY_ID, OrderDir.ASC, false);
         }
     }
 }
