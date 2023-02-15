29fe3249b83cb35109c8274fd4d1c3d18b7ef2c9
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-15 16:38:04 +0200

    added tests for MyProfileCommand

---
+++ src/test/java/com/my/library/controller/command/impl/user/MyProfileCommandTest.java
@@ -0,0 +1,56 @@
+package com.my.library.controller.command.impl.user;
+
+import com.my.library.controller.command.CommandResult;
+import com.my.library.controller.command.constant.CommandDirection;
+import com.my.library.controller.command.constant.RedirectToPage;
+import com.my.library.controller.command.constant.parameters.Parameters;
+import com.my.library.controller.command.constant.parameters.UserParameters;
+import com.my.library.dto.UserDTO;
+import com.my.library.entities.User;
+import com.my.library.exceptions.CommandException;
+import com.my.library.services.BookService;
+import com.my.library.services.OrderService;
+import com.my.library.services.UserService;
+import com.my.library.utils.Pages;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.apache.logging.log4j.Level;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class MyProfileCommandTest {
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private HttpSession session;
+    @Mock
+    private MyProfileCommand myProfileCommand;
+
+
+    @Test
+    void execute_ShouldReturnCommandResultWithForwardToMyProfilePage() throws CommandException {
+        UserDTO userDTO = mock(UserDTO.class);
+        doReturn(session).when(request).getSession();
+        doReturn(userDTO).when(session).getAttribute(UserParameters.USER_IN_SESSION);
+
+        var result = myProfileCommand.execute(request);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getAction()).isEqualTo(CommandDirection.FORWARD);
+        assertThat(result.getPage()).isEqualTo(Pages.MY_PROFILE);
+
+        verify(request).getSession();
+        verify(request).setAttribute(Parameters.USER_DTO, userDTO);
+        verify(session).setAttribute(Parameters.PREVIOUS_PAGE, RedirectToPage.MY_PROFILE);
+    }
+
+}
