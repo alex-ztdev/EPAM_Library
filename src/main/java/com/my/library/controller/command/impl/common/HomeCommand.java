1f64e15fc85f588ced00fd2f71c631175abfb0ea
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-15 19:37:32 +0200

    refactored code

--- src/main/java/com/my/library/controller/command/impl/common/HomeCommand.java
+++ src/main/java/com/my/library/controller/command/impl/common/HomeCommand.java
@@ -2,19 +2,21 @@ package com.my.library.controller.command.impl.common;
 
 import com.my.library.controller.command.Command;
 import com.my.library.controller.command.CommandResult;
+import com.my.library.controller.command.constant.RedirectToPage;
 import com.my.library.controller.command.constant.parameters.Parameters;
 import com.my.library.exceptions.CommandException;
 import com.my.library.utils.Pages;
 import com.my.library.utils.validator.MessagesRemover;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 public class HomeCommand implements Command {
 
     @Override
     public CommandResult execute(HttpServletRequest request) throws CommandException {
-        var session = request.getSession();
+        HttpSession session = request.getSession();
         new MessagesRemover().removeLoginErrors(session);
-        session.setAttribute(Parameters.PREVIOUS_PAGE, Pages.MAIN_PAGE);
+        session.setAttribute(Parameters.PREVIOUS_PAGE, RedirectToPage.HOME);
         return new CommandResult(Pages.MAIN_PAGE);
     }
 }
