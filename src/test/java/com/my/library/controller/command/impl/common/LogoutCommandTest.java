2497f1adce4335259214ca6fd8e0afdf83da2421
Alex Horobets <sgs.cat.fly@gmail.com>
2023-02-15 19:32:26 +0200

    added removeAttribute verification

--- src/test/java/com/my/library/controller/command/impl/common/LogoutCommandTest.java
+++ src/test/java/com/my/library/controller/command/impl/common/LogoutCommandTest.java
@@ -4,6 +4,7 @@ import com.my.library.controller.command.CommandResult;
 import com.my.library.controller.command.constant.CommandDirection;
 import com.my.library.controller.command.constant.RedirectToPage;
 import com.my.library.controller.command.constant.parameters.Parameters;
+import com.my.library.controller.command.constant.parameters.UserParameters;
 import com.my.library.exceptions.CommandException;
 import com.my.library.utils.Pages;
 import jakarta.servlet.http.HttpServletRequest;
@@ -39,5 +40,6 @@ class LogoutCommandTest {
 
         verify(request).getSession();
         verify(session).setAttribute(Parameters.PREVIOUS_PAGE, RedirectToPage.LOGIN_PAGE);
+        verify(session).removeAttribute(UserParameters.USER_IN_SESSION);
     }
 }
