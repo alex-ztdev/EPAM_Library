9993e52b75fd87771f486a5bb4e3487ada0db250
Alex Horobets <sgs.cat.fly@gmail.com>
2023-03-04 22:29:25 +0200

    removed unused commands

--- src/main/java/com/my/library/controller/filter/AuthenticationFilter.java
+++ src/main/java/com/my/library/controller/filter/AuthenticationFilter.java
@@ -7,11 +7,15 @@ import com.my.library.controller.command.constant.commands.LibrarianCommands;
 import com.my.library.controller.command.constant.commands.UserCommands;
 import com.my.library.controller.command.constant.parameters.Parameters;
 import com.my.library.controller.command.constant.parameters.UserParameters;
-import com.my.library.controller.command.context.AppContext;
+import com.my.library.controller.context.AppContext;
 import com.my.library.dao.constants.UserRole;
 import com.my.library.dto.UserDTO;
 import com.my.library.exceptions.ServiceException;
-import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -21,28 +25,28 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @WebFilter("/controller")
 public class AuthenticationFilter implements Filter {
     private static final Logger logger = LogManager.getLogger();
-    private static final List<String> GENERAL_COMMANDS = List.of(
+    private static final Set<String> GENERAL_COMMANDS = new HashSet<>(List.of(
             GeneralCommands.CHANGE_LANGUAGE,
             GeneralCommands.LOGIN,
             GeneralCommands.LOGIN_PAGE,
             GeneralCommands.LOGOUT,
             GeneralCommands.REGISTRATION,
-            GeneralCommands.ALL_BOOKS_LIST,
             GeneralCommands.BOOKS_LIST,
             GeneralCommands.SEARCH_BOOK,
-            GeneralCommands.SORT_BOOKS,
             GeneralCommands.HOME,
             GeneralCommands.NOT_AUTHORIZED,
             GeneralCommands.UNSUPPORTED_OPERATION,
             GeneralCommands.ERROR_PAGE
-    );
+    ));
 
-    private static final List<String> LIBRARIAN_COMMANDS = List.of(
+    private static final Set<String> LIBRARIAN_COMMANDS = new HashSet<>(List.of(
             LibrarianCommands.DISPLAY_USERS_ORDERS,
             LibrarianCommands.RETURN_ORDER,
             LibrarianCommands.DISPLAY_READERS,
@@ -51,18 +55,18 @@ public class AuthenticationFilter implements Filter {
             LibrarianCommands.DECLINE_ORDER,
 
             UserCommands.MY_PROFILE
-    );
+    ));
 
-    private static final List<String> USER_COMMANDS = List.of(
+    private static final Set<String> USER_COMMANDS = new HashSet<>(List.of(
             UserCommands.ORDER_BOOK_REDIRECT,
             UserCommands.ORDER_BOOK,
             UserCommands.DISPLAY_MY_ORDERS,
             UserCommands.MY_PROFILE,
             UserCommands.DISPLAY_MY_REQUESTS,
             UserCommands.CANCEL_ORDER
-    );
+    ));
 
-    private static final List<String> ADMIN_COMMANDS = List.of(
+    private static final Set<String> ADMIN_COMMANDS = new HashSet<>(List.of(
             AdminCommands.REMOVE_BOOK,
             AdminCommands.RESTORE_BOOK,
             AdminCommands.ADD_BOOK_REDIRECT,
@@ -82,50 +86,54 @@ public class AuthenticationFilter implements Filter {
             LibrarianCommands.RETURN_ORDER,
             LibrarianCommands.ACCEPT_ORDER,
             LibrarianCommands.DECLINE_ORDER
-
-    );
+    ));
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-
         HttpSession session = request.getSession();
-        UserDTO user = (UserDTO) session.getAttribute(UserParameters.USER_IN_SESSION);
-
         String command = request.getParameter(GeneralCommands.COMMAND_PARAMETER);
 
-        if (command == null || !ADMIN_COMMANDS.contains(command) && !USER_COMMANDS.contains(command) && !GENERAL_COMMANDS.contains(command) && !LIBRARIAN_COMMANDS.contains(command)) {
+        if (command == null || !ADMIN_COMMANDS.contains(command)
+                && !USER_COMMANDS.contains(command)
+                && !GENERAL_COMMANDS.contains(command)
+                && !LIBRARIAN_COMMANDS.contains(command)
+        ) {
             logger.log(Level.DEBUG, "Unknown command: " + command + " was received");
             session.setAttribute(Parameters.PREVIOUS_PAGE, RedirectToPage.UNSUPPORTED_OPERATION);
             response.sendRedirect(request.getContextPath() + RedirectToPage.UNSUPPORTED_OPERATION);
-        } else if (GENERAL_COMMANDS.contains(command)) {
-            chain.doFilter(servletRequest, servletResponse);
-        } else {
+        } else if (!GENERAL_COMMANDS.contains(command)) {
+            UserDTO user = (UserDTO) session.getAttribute(UserParameters.USER_IN_SESSION);
             if (user == null) {
                 session.setAttribute(Parameters.PREVIOUS_PAGE, RedirectToPage.NOT_AUTHORIZED);
                 response.sendRedirect(request.getContextPath() + RedirectToPage.NOT_AUTHORIZED);
+                chain.doFilter(request, response);
                 logger.log(Level.DEBUG, "No user in session! Tried to execute: " + command);
             } else {
                 if (isUserBanned(user)) {
                     logger.log(Level.DEBUG, "User with id: " + user.getUserId() + " is banned! Invoked logout command");
                     session.invalidate();
                     response.sendRedirect(request.getContextPath() + RedirectToPage.LOGIN_PAGE);
+                    chain.doFilter(request, response);
                 } else if (user.getRole() == UserRole.LIBRARIAN && LIBRARIAN_COMMANDS.contains(command)) {
                     logger.log(Level.DEBUG, "Librarian: " + user.getUserId() + " executed: " + command);
-                    chain.doFilter(servletRequest, servletResponse);
+                    chain.doFilter(request, response);
                 } else if (user.getRole() == UserRole.USER && USER_COMMANDS.contains(command)) {
                     logger.log(Level.DEBUG, "User: " + user.getUserId() + " executed: " + command);
-                    chain.doFilter(servletRequest, servletResponse);
+                    chain.doFilter(request, response);
                 } else if (user.getRole() == UserRole.ADMIN && ADMIN_COMMANDS.contains(command)) {
                     logger.log(Level.DEBUG, "Admin: " + user.getUserId() + " executed: " + command);
-                    chain.doFilter(servletRequest, servletResponse);
+                    chain.doFilter(request, response);
                 } else {
                     session.setAttribute(Parameters.PREVIOUS_PAGE, RedirectToPage.NOT_AUTHORIZED);
                     response.sendRedirect(request.getContextPath() + RedirectToPage.NOT_AUTHORIZED);
+                    chain.doFilter(request, response);
                 }
             }
+        } else {
+            chain.doFilter(request, response);
         }
     }
 
